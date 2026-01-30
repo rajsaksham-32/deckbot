506d316a20047655767b8842a92834a2af79bd2f
Saksham Raj <gyanskraj32@gmail.com>
2026-01-30 21:32:07 +0530

    Fix import path for deployment

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from discord import app_commands
 import os
 from dotenv import load_dotenv
 
-from src.data.deck import draw_card
+from data.deck import draw_card
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
